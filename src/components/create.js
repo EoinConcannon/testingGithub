057ceba6232554fa635c55d04bcf00aca7f6c798
EoinConcannon <G00406729@atu.ie>
2023-10-20 09:38:08 +0000

    lab 4

--- src/components/create.js
+++ src/components/create.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+
 function Create() {
 
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [cover, setCover] = useState('');
+
+    const handleSubmit = (e) => {//when the submit button is clicked
+        e.preventDefault();//stops being called multiple times
+
+        console.log("Title: " + title + "\nAuthor: " + author + "\nCover: " + cover)//displays user inputs to console
+    }
     return (
         <div>
             <h3>Hello from my create component!</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Book Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}//invokes title and assigns the text to title
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Book Author: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Book Cover: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => { setCover(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Add Book"></input>
+                </div>
+
+            </form>
         </div>
     );
 }
--- src/components/read.js
+++ src/components/read.js
@@ -1,42 +1,28 @@
+import { useEffect, useState } from "react";//react hook
 import Books from "./books";
+import axios from "axios";
 
 function Read() {
 
-    const data = [
-        {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-        },
-        {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-                "Kyle Banker",
-                "Peter Bakkum",
-                "Tim Hawkins",
-                "Shaun Verch",
-                "Douglas Garrett"
-            ],
-            "categories": []
-        },
-        {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-        }
-    ];
+    const [data, setData] = useState([]);//gives access to state object
+
+    useEffect(
+        () => {
+            //async web development
+            //gets json data, axios doesn't stall website while getting data
+            axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')//promise
+                .then(//data was found
+                    (response) => {
+                        setData(response.data.books);//uses array name in json file
+                    }
+                )
+                .catch(//data was NOT found
+                    (error) => {
+                        console.log(error);
+                    }
+                )
+        }, []//empty array stops repeating
+    );
 
     return (
         <div>
